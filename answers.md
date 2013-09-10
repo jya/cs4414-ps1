3ddb9edb9d19c672fb81568a9d35c4a4cd3f109d
jy2an <jy2an@virginia.edu>
2013-09-10 17:26:54 -0400

    Adding answers.md and problem 3

---
+++ answers.md
@@ -0,0 +1,14 @@
+Title: Problem Set 1 Answers
+Author: Jason Ya
+
+1. Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:23.0) Gecko/20100101 Firefox/23.0
+
+Mozilla/5.0 reports a Mozilla-based user agent, and Firefox is within that group. The number represents the Mozilla version.
+
+X11 refers to the X windowing system used as the system-level software infrastructure for the windowing GUI on Linux. Ubuntu refers to the distribution used on my VM. Linux i686 refers to a Linux OS running on an processor with i686 architecture. rv:23.0 means that the browser is running version 23.0 of Gecko.
+
+Gecko/20100101 signifies the Gecko engine and the browser's build date.
+
+Firefox/23.0 says I am using Firefox version 23.0.
+
+2. Because the variable is global, it can be modified by any function in the class. Unless the accesses/updates to that variable are managed, concurrent accesses/updates can result in inaccurate values.
--- zhttpto.rs
+++ zhttpto.rs
@@ -14,6 +14,7 @@ use std::str;
 static BACKLOG: uint = 5;
 static PORT:    uint = 4414;
 static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static mut count : uint = 0;
 
 fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::comm::SharedChan<Option<extra::net_tcp::TcpErrData>>)
 {
@@ -26,6 +27,7 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
             Ok(sock) => {
                 let peer_addr: ~str = net_ip::format_addr(&sock.get_peer_addr());
                 println(fmt!("Received connection from: %s", peer_addr));
+		unsafe{ count += 1; }
                 
                 let read_result = net_tcp::read(&sock, 0u);
                 match read_result {
@@ -35,15 +37,22 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
                     Ok(bytes) => {
                         let request_str = str::from_bytes(bytes.slice(0, bytes.len() - 1));
                         println(fmt!("Request received:\n%s", request_str));
-                        let response: ~str = ~
+			unsafe{ println(fmt!("Number of requests: %u", count)); }
+                        let mut response: ~str = ~
                             "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                              <doctype !html><html><head><title>Hello, Rust!</title>
                              <style>body { background-color: #111; color: #FFEEAA }
                                     h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
                              </style></head>
                              <body>
-                             <h1>Greetings, Rusty!</h1>
-                             </body></html>\r\n";
+                             <h1>Greetings, Rusty!</h1>";
+			unsafe{
+			let test = fmt!("<h2>Number of requests: %u</h2></body></html>\r\n", count);
+			println(test);}
+			unsafe{ response = response + fmt!(
+					"<h2>Number of requests: %u</h2>
+					</body></html>\r\n", count); }
+			println(response);
 
                         net_tcp::write(&sock, response.as_bytes_with_null_consume());
                     },
